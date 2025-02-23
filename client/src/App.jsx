6cf962a0f22aec2f35a6451046f3c08767c037d0
shahriar-sohan <sohanineu@gmail.com>
2025-02-23 20:37:21 +0000

    shadecn bug fixed

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+        <div>
+          <h1 className='text-amber-300'>boom boom</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose =  require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+mongoose.connect('mongodb+srv://rivaloncode:1943Sohan@cluster0.29byc.mongodb.net/')
+
+.then(()=>console.log('mongodb connected successful'))
+.catch(error=>console.log(error))
+
+
+const app = express()
+const PORT = process.env.PORT || 5001 ;
+
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET','POST','DELETE','PUT'],
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma"
+    ],credentials: true
+}))
+
+app.use(cookieParser())
+app.use(express.json())
+
+
+app.listen(PORT,()=>console.log(`server is runnin on port: ${PORT}`))
